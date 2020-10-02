58c898cdb3ad7df250ebea742e432f97b6cbfe31
OlgaFedorovich <63749183+OlgaFedorovich@users.noreply.github.com>
2020-10-02 12:12:30 +0300

    feat:solve array problems

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 
 exports.min = function min (array) {
-  return 0;
-}
+  if(!array || array.length === 0) return 0;
+  let sortedArray = array.sort((a,b) => (a-b));
+  return sortedArray[0];
+};
 
 exports.max = function max (array) {
-  return 0;
-}
+  if(!array || array.length === 0) return 0;
+  let sortedArray = array.sort((a, b) => (a - b));
+  return sortedArray[array.length - 1];
+};
 
 exports.avg = function avg (array) {
-  return 0;
-}
+  if(!array || array.length === 0) return 0;
+  return array.reduce((acc, element) => (acc + element)) / array.length;
+};
+
